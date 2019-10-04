2420ebd041170a5f4e55c8a82ab0f9b7bc3c8c14
Grig Alex <alexengrigdev@gmail.com>
2019-10-04 20:50:23 +0300

    Create DearSweetResultSet with simple implementation

---
+++ src/main/java/ru/spb/devclub/utils/db/DearSweetResultSet.java
@@ -0,0 +1,29 @@
+package ru.spb.devclub.utils.db;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.OptionalLong;
+
+public class DearSweetResultSet extends AbstractSweetResultSet {
+    protected DearSweetResultSet(ResultSet resultSet) {
+        super(resultSet);
+    }
+
+    @Override
+    public Long getLongOrNull(String columnName) throws SQLException {
+        long value = resultSet.getLong(columnName);
+        return resultSet.wasNull() ? null : value;
+    }
+
+    @Override
+    public Long getLongOrDefault(String columnName, long defaultValue) throws SQLException {
+        long value = resultSet.getLong(columnName);
+        return resultSet.wasNull() ? defaultValue : value;
+    }
+
+    @Override
+    public OptionalLong getOptionalLong(String columnName) throws SQLException {
+        long value = resultSet.getLong(columnName);
+        return resultSet.wasNull() ? OptionalLong.empty() : OptionalLong.of(value);
+    }
+}
