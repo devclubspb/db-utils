39e7d1448834b79069789cab34ce840cb84e65dc
Grig Alex <alexengrigdev@gmail.com>
2019-10-09 11:02:53 +0300

    Add optional get methods by column index

--- src/main/java/ru/spb/devclub/utils/db/OptionalResultSet.java
+++ src/main/java/ru/spb/devclub/utils/db/OptionalResultSet.java
@@ -1,7 +1,10 @@
 package ru.spb.devclub.utils.db;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
 import java.util.OptionalLong;
 
 /**
@@ -11,11 +14,45 @@ import java.util.OptionalLong;
  * @version 0.1.0
  * @see java.sql.ResultSet
  * @see java.util.Optional
+ * @see java.util.OptionalDouble
+ * @see java.util.OptionalInt;
  * @see java.util.OptionalLong
  * @since 0.1.0
  */
 public interface OptionalResultSet extends ResultSet {
+    Optional<String> getOptionalString(int columnIndex) throws SQLException;
+
+    Optional<Boolean> getOptionalBoolean(int columnIndex) throws SQLException;
+
+    Optional<Byte> getOptionalByte(int columnIndex) throws SQLException;
+
+    Optional<Short> getOptionalShort(int columnIndex) throws SQLException;
+
+    OptionalInt getOptionalInt(int columnIndex) throws SQLException;
+
     OptionalLong getOptionalLong(String columnName) throws SQLException;
 
     OptionalLong getOptionalLong(int columnIndex) throws SQLException;
+
+    Optional<Float> getOptionalFloat(int columnIndex) throws SQLException;
+
+    OptionalDouble getOptionalDouble(int columnIndex) throws SQLException;
+
+    Optional<byte[]> getOptionalBytes(int columnIndex) throws SQLException;
+
+    Optional<Date> getOptionalDate(int columnIndex) throws SQLException;
+
+    Optional<Time> getOptionalTime(int columnIndex) throws SQLException;
+
+    Optional<Timestamp> getOptionalTimestamp(int columnIndex) throws SQLException;
+
+    Optional<BigDecimal> getOptionalBigDecimal(int columnIndex) throws SQLException;
+
+    Optional<Ref> getOptionalRef(int columnIndex) throws SQLException;
+
+    Optional<Blob> getOptionalBlob(int columnIndex) throws SQLException;
+
+    Optional<Clob> getOptionalClob(int columnIndex) throws SQLException;
+
+    Optional<Array> getOptionalArray(int columnIndex) throws SQLException;
 }
