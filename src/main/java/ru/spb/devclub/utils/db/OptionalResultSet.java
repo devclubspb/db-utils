72e4e92a2bff67d2329934c1a315ecc9c5db38f4
Grig Alex <alexengrigdev@gmail.com>
2019-10-10 10:43:36 +0300

    Add optional get methods by column name

--- src/main/java/ru/spb/devclub/utils/db/OptionalResultSet.java
+++ src/main/java/ru/spb/devclub/utils/db/OptionalResultSet.java
@@ -22,37 +22,69 @@ import java.util.OptionalLong;
 public interface OptionalResultSet extends ResultSet {
     Optional<String> getOptionalString(int columnIndex) throws SQLException;
 
+    Optional<String> getOptionalString(String columnName) throws SQLException;
+
     Optional<Boolean> getOptionalBoolean(int columnIndex) throws SQLException;
+
+    Optional<Boolean> getOptionalBoolean(String columnName) throws SQLException;
 
     Optional<Byte> getOptionalByte(int columnIndex) throws SQLException;
 
+    Optional<Byte> getOptionalByte(String columnName) throws SQLException;
+
     Optional<Short> getOptionalShort(int columnIndex) throws SQLException;
+
+    Optional<Short> getOptionalShort(String columnName) throws SQLException;
 
     OptionalInt getOptionalInt(int columnIndex) throws SQLException;
 
-    OptionalLong getOptionalLong(String columnName) throws SQLException;
+    OptionalInt getOptionalInt(String columnName) throws SQLException;
 
     OptionalLong getOptionalLong(int columnIndex) throws SQLException;
 
+    OptionalLong getOptionalLong(String columnName) throws SQLException;
+
     Optional<Float> getOptionalFloat(int columnIndex) throws SQLException;
+
+    Optional<Float> getOptionalFloat(String columnName) throws SQLException;
 
     OptionalDouble getOptionalDouble(int columnIndex) throws SQLException;
 
+    OptionalDouble getOptionalDouble(String columnName) throws SQLException;
+
     Optional<byte[]> getOptionalBytes(int columnIndex) throws SQLException;
+
+    Optional<byte[]> getOptionalBytes(String columnName) throws SQLException;
 
     Optional<Date> getOptionalDate(int columnIndex) throws SQLException;
 
+    Optional<Date> getOptionalDate(String columnName) throws SQLException;
+
     Optional<Time> getOptionalTime(int columnIndex) throws SQLException;
+
+    Optional<Time> getOptionalTime(String columnName) throws SQLException;
 
     Optional<Timestamp> getOptionalTimestamp(int columnIndex) throws SQLException;
 
+    Optional<Timestamp> getOptionalTimestamp(String columnName) throws SQLException;
+
     Optional<BigDecimal> getOptionalBigDecimal(int columnIndex) throws SQLException;
+
+    Optional<BigDecimal> getOptionalBigDecimal(String columnName) throws SQLException;
 
     Optional<Ref> getOptionalRef(int columnIndex) throws SQLException;
 
+    Optional<Ref> getOptionalRef(String columnName) throws SQLException;
+
     Optional<Blob> getOptionalBlob(int columnIndex) throws SQLException;
+
+    Optional<Blob> getOptionalBlob(String columnName) throws SQLException;
 
     Optional<Clob> getOptionalClob(int columnIndex) throws SQLException;
 
+    Optional<Clob> getOptionalClob(String columnName) throws SQLException;
+
     Optional<Array> getOptionalArray(int columnIndex) throws SQLException;
+
+    Optional<Array> getOptionalArray(String columnName) throws SQLException;
 }
