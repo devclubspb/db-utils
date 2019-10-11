6b9a326b09899add14c0b43dced1f4c9118c0b71
vechis <vechis@mail.ru>
2019-10-11 10:04:11 +0300

    add methods to BoxedResultSet for getting values
    Integer, Boolean, Timestamp, Date, DateString

--- src/main/java/ru/spb/devclub/utils/db/ResultSetUtils.java
+++ src/main/java/ru/spb/devclub/utils/db/ResultSetUtils.java
@@ -11,22 +11,39 @@ public final class ResultSetUtils {
     private ResultSetUtils() {
     }
 
-    public static <T> T getEnumValue(ResultSet rs, String columnName, T[] enumItems) throws SQLException {
+    public static <T extends Enum<T>> T getEnumValue(ResultSet rs, String columnName, T[] enumItems) throws SQLException {
         String result = rs.getString(columnName);
         if (!rs.wasNull()) {
-            for (T item : enumItems) {
-                if (item.toString().equals(result)) {
-                    return item;
-                }
-            }
-            throw new IllegalArgumentException("Enum \"" + enumItems.getClass().getComponentType().getName() +
-                    "\" doesn't contain a constant by the string value \"" + result + "\".");
+            return getEnumValue(result, enumItems);
         }
         return null;
     }
 
+    public static <T extends Enum<T>> T getEnumValue(ResultSet rs, int columnIndex, T[] enumItems) throws SQLException {
+        String result = rs.getString(columnIndex);
+        if (!rs.wasNull()) {
+            return getEnumValue(result, enumItems);
+        }
+        return null;
+    }
+
+    private static <T extends Enum<T>> T getEnumValue(String value, T[] enumItems) {
+        for (T item : enumItems) {
+            if (item.toString().equals(value)) {
+                return item;
+            }
+        }
+        throw new IllegalArgumentException("Enum \"" + enumItems.getClass().getComponentType().getName() +
+                "\" doesn't contain a constant by the string value \"" + value + "\".");
+    }
+
     public static Long getLong(ResultSet rs, String columnName) throws SQLException {
         long result = rs.getLong(columnName);
+        return !rs.wasNull() ? result : null;
+    }
+
+    public static Long getLong(ResultSet rs, int columnIndex) throws SQLException {
+        long result = rs.getLong(columnIndex);
         return !rs.wasNull() ? result : null;
     }
 
@@ -35,8 +52,18 @@ public final class ResultSetUtils {
         return !rs.wasNull() ? result : null;
     }
 
+    public static Integer getInt(ResultSet rs, int columnIndex) throws SQLException {
+        int result = rs.getInt(columnIndex);
+        return !rs.wasNull() ? result : null;
+    }
+
     public static Boolean getBoolean(ResultSet rs, String columnName) throws SQLException {
         boolean result = rs.getBoolean(columnName);
+        return !rs.wasNull() ? result : null;
+    }
+
+    public static Boolean getBoolean(ResultSet rs, int columnIndex) throws SQLException {
+        boolean result = rs.getBoolean(columnIndex);
         return !rs.wasNull() ? result : null;
     }
 
@@ -45,13 +72,28 @@ public final class ResultSetUtils {
         return !rs.wasNull() ? result : null;
     }
 
+    public static Timestamp getTimestamp(ResultSet rs, int columnIndex) throws SQLException {
+        Timestamp result = rs.getTimestamp(columnIndex);
+        return !rs.wasNull() ? result : null;
+    }
+
     public static Date getDate(ResultSet rs, String columnName) throws SQLException {
         Date result = rs.getDate(columnName);
         return !rs.wasNull() ? result : null;
     }
 
+    public static Date getDate(ResultSet rs, int columnIndex) throws SQLException {
+        Date result = rs.getDate(columnIndex);
+        return !rs.wasNull() ? result : null;
+    }
+
     public static String getDateString(ResultSet rs, String columnName, DateFormat dateFormat) throws SQLException {
         Date date = rs.getDate(columnName);
+        return !rs.wasNull() ? dateFormat.format(date) : null;
+    }
+
+    public static String getDateString(ResultSet rs, int columnIndex, DateFormat dateFormat) throws SQLException {
+        Date date = rs.getDate(columnIndex);
         return !rs.wasNull() ? dateFormat.format(date) : null;
     }
 
--- src/main/java/ru/spb/devclub/utils/db/SimpleBoxedResultSet.java
+++ src/main/java/ru/spb/devclub/utils/db/SimpleBoxedResultSet.java
@@ -1,7 +1,10 @@
 package ru.spb.devclub.utils.db;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.DateFormat;
 
 /* package */class SimpleBoxedResultSet extends ProxyResultSet implements BoxedResultSet {
     @SuppressWarnings("WeakerAccess")
@@ -10,14 +13,72 @@ import java.sql.SQLException;
     }
 
     @Override
+    public <T extends Enum<T>> T getEnumValue(String columnName, T[] enumItems) throws SQLException {
+        return ResultSetUtils.getEnumValue(resultSet, columnName, enumItems);
+    }
+
+    @Override
+    public <T extends Enum<T>> T getEnumValue(int columnIndex, T[] enumItems) throws SQLException {
+        return ResultSetUtils.getEnumValue(resultSet, columnIndex, enumItems);
+    }
+
+    @Override
     public Long getLongOrNull(String columnName) throws SQLException {
-        long value = resultSet.getLong(columnName);
-        return resultSet.wasNull() ? null : value;
+        return ResultSetUtils.getLong(resultSet, columnName);
     }
 
     @Override
     public Long getLongOrNull(int columnIndex) throws SQLException {
-        long value = resultSet.getLong(columnIndex);
-        return resultSet.wasNull() ? null : value;
+        return ResultSetUtils.getLong(resultSet, columnIndex);
+    }
+
+    @Override
+    public Integer getIntOrNull(String columnName) throws SQLException {
+        return ResultSetUtils.getInt(resultSet, columnName);
+    }
+
+    @Override
+    public Integer getIntOrNull(int columnIndex) throws SQLException {
+        return ResultSetUtils.getInt(resultSet, columnIndex);
+    }
+
+    @Override
+    public Boolean getBooleanOrNull(String columnName) throws SQLException {
+        return ResultSetUtils.getBoolean(resultSet, columnName);
+    }
+
+    @Override
+    public Boolean getBooleanOrNull(int columnIndex) throws SQLException {
+        return ResultSetUtils.getBoolean(resultSet, columnIndex);
+    }
+
+    @Override
+    public Timestamp getTimestampOrNull(String columnName) throws SQLException {
+        return ResultSetUtils.getTimestamp(resultSet, columnName);
+    }
+
+    @Override
+    public Timestamp getTimestampOrNull(int columnIndex) throws SQLException {
+        return ResultSetUtils.getTimestamp(resultSet, columnIndex);
+    }
+
+    @Override
+    public Date getDateOrNull(String columnName) throws SQLException {
+        return ResultSetUtils.getDate(resultSet, columnName);
+    }
+
+    @Override
+    public Date getDateOrNull(int columnIndex) throws SQLException {
+        return ResultSetUtils.getDate(resultSet, columnIndex);
+    }
+
+    @Override
+    public String getDateStringOrNull(String columnName, DateFormat dateFormat) throws SQLException {
+        return ResultSetUtils.getDateString(resultSet, columnName, dateFormat);
+    }
+
+    @Override
+    public String getDateStringOrNull(int columnIndex, DateFormat dateFormat) throws SQLException {
+        return ResultSetUtils.getDateString(resultSet, columnIndex, dateFormat);
     }
 }
