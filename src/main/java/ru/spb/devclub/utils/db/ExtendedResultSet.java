bca46e82bf39e98e395abd0b1a32b184db9eb955
Grig Alex <alexengrigdev@gmail.com>
2019-10-14 14:37:47 +0300

    Adds ExtendedResultSet

---
+++ src/main/java/ru/spb/devclub/utils/db/ExtendedResultSet.java
@@ -0,0 +1,14 @@
+package ru.spb.devclub.utils.db;
+
+import java.sql.ResultSet;
+
+/**
+ * Extends {@link java.sql.ResultSet} interface.
+ *
+ * @author Grig Alex
+ * @version 0.1.0
+ * @see java.sql.ResultSet
+ * @since 0.1.0
+ */
+public interface ExtendedResultSet extends ResultSet {
+}
