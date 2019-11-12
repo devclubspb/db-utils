4b8d97874a6db1d9ab2659cc9b1983867540c3b1
Grig Alex <alexengrigdev@gmail.com>
2019-11-12 09:44:52 +0300

    Refactors BaseResultSetMockTest

--- src/test/java/ru/spb/devclub/utils/db/BaseResultSetMockTest.java
+++ src/test/java/ru/spb/devclub/utils/db/BaseResultSetMockTest.java
@@ -1,17 +1,13 @@
 package ru.spb.devclub.utils.db;
 
-import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.sql.ResultSet;
 
+@RunWith(MockitoJUnitRunner.class)
 public abstract class BaseResultSetMockTest {
     @Mock
     protected ResultSet mockResultSet;
-
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
 }
